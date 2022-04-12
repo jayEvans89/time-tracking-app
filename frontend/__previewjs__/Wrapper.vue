3e1f12fb44b7b3ce8265ac6c396d81a9e5e15bf5
Jay Evans <james@cloudskyinc.com>
2022-04-12 16:40:41 +0100

    Adds wrapper for preview.js

---
+++ frontend/__previewjs__/Wrapper.vue
@@ -0,0 +1,21 @@
+<template>
+  <slot />
+</template>
+
+<script lang="ts" setup>
+import { createPinia } from 'pinia'
+import { getCurrentInstance } from 'vue'
+
+import router from '../src/core/router'
+
+const pinia = createPinia()
+const { appContext } = getCurrentInstance()
+appContext.app
+  .use(pinia)
+  .use(router)
+</script>
+
+<style lang="scss">
+@use '@styles/main';
+
+</style>
